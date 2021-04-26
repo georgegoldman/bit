697993329de9a409e613fa55f4478bd1b852c954
georgegoldman <georgegoldman2014@gmail.com>
2021-04-27 00:03:03 +0100

    allthing good

---
+++ application/route/user.py
@@ -0,0 +1,18 @@
+import os, secrets, cloudinary.uploader, cloudinary.api, cloudinary
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, login_user, logout_user, current_user
+from application import db, bcrypt, allowed_file, app
+from application.model.user import User
+from application.model.menus import Menus
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+user = Blueprint('user',__name__)
+
+@user.route('/')
+def userery():
+    return 'bit'
+
+
+
+
